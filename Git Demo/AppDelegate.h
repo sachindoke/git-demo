eb7e61bc1cf29b06c4408f24f05f846ffc25fb38
apple <apple@apples-MacBook-Air-2.local>
2016-07-31 14:08:44 +0530

    message

---
+++ Git Demo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Git Demo
+//
+//  Created by apple on 31/07/16.
+//  Copyright © 2016 felix-its. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Git Demo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Git Demo
+//
+//  Created by apple on 31/07/16.
+//  Copyright © 2016 felix-its. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
